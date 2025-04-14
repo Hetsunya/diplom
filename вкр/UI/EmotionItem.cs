1e12013d5cf84b71b53a60047736ac4a730398c2
Hetsunya <99246859+Hetsunya@users.noreply.github.com>
2025-04-14 13:38:05 +0500

    закончил

---
+++ вкр/UI/EmotionItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI
+{
+    public class EmotionItem
+    {
+        public string Emotion { get; set; }
+        public string Probability { get; set; }
+    }
+}
