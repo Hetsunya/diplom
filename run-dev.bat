9dd2ff96a8ea45987b3e17a8917eef4fb06b1110
Hetsunya <99246859+Hetsunya@users.noreply.github.com>
2026-06-10 01:03:00 +0500

    почистил вне code

--- run-dev.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM === Backend ===
-start "emeeting-backend" cmd /k ^
-cd /d "%~dp0code\emeeting-backend" ^&^& go run .\cmd\server\main.go
-
-REM === Frontend ===
-start "emeeting-ui" cmd /k ^
-cd /d "%~dp0code\emeeting-ui" ^&^& npm run dev
